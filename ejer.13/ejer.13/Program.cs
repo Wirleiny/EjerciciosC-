dd3400f68a3aefd97db6f766c4a8ebef50ce6af2
Wirleiny <wirleinycruzpolanco@gmail.com>
2024-11-09 22:08:26 -0400

    Add files via upload

---
+++ ejer.13/ejer.13/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ejer._13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa un número:");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine(numero + " es un número par.");
+            }
+            else
+            {
+                Console.WriteLine(numero + " es un número impar.");
+            }
+        }
+    }
+}
