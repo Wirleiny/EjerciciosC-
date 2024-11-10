1a38a65d350b63c47b02b871b931fd67c4fd7b32
Wirleiny <wirleinycruzpolanco@gmail.com>
2024-11-09 22:11:27 -0400

    Add files via upload

---
+++ ejer.18/ejer.18/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ejer._18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la producción del lunes:");
+            double produccionLunes = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la producción del martes:");
+            double produccionMartes = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la producción del miércoles:");
+            double produccionMiércoles = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la producción del jueves:");
+            double produccionJueves = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la producción del viernes:");
+            double produccionViernes = Convert.ToDouble(Console.ReadLine());
+
+            double totalProduccion = produccionLunes + produccionMartes + produccionMiércoles + produccionJueves + produccionViernes;
+            double promedioProduccion = totalProduccion / 5;
+            double incentivo = 0;
+
+            if (promedioProduccion >= 20000)
+            {
+                incentivo = 3000;
+            }
+
+            Console.WriteLine("Producción total: " + totalProduccion);
+            Console.WriteLine("Promedio de producción: " + promedioProduccion);
+            Console.WriteLine("Incentivo: " + incentivo);
+        }
+    }
+}
+    
+
