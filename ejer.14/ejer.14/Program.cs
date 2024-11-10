ac0eb3ca02010c80773ced068e74a74cdf1b748e
Wirleiny <wirleinycruzpolanco@gmail.com>
2024-11-09 22:08:54 -0400

    Add files via upload

---
+++ ejer.14/ejer.14/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ejer._14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce el primer número: ");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Introduce el segundo número: ");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            if (numero1 > numero2)
+            {
+                Console.WriteLine("El número mayor es: " + numero1);
+            }
+            else if (numero2 > numero1)
+            {
+                Console.WriteLine("El número mayor es: " + numero2);
+            }
+            else
+            {
+                Console.WriteLine("Ambos números son iguales");
+            }
+        }
+    }
+}
+    
+
