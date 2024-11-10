ca4aa463120dc48cc7de0a70aa2816ccc0d0732e
Wirleiny <wirleinycruzpolanco@gmail.com>
2024-11-09 22:12:28 -0400

    Add files via upload

---
+++ ejer.19/ejer.19/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ejer._19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el sueldo del empleado:");
+            double sueldo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el número de hijos:");
+            int numeroHijos = Convert.ToInt32(Console.ReadLine());
+
+            double incentivo = 0;
+
+            if (numeroHijos > 0)
+            {
+                incentivo = numeroHijos * 500;
+            }
+
+            Console.WriteLine("Sueldo: " + sueldo);
+            Console.WriteLine("Número de hijos: " + numeroHijos);
+            Console.WriteLine("Incentivo total: " + incentivo);
+        }
+    }
+}
+    
+
